013b16b637a271be21b797049a41e07d41064984
myself659 <myself659@gmail.com>
2021-10-17 21:14:20 +0800

    dotenv-http-param-get

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
 use std::collections::HashMap;
 
 #[tokio::main]
-async  fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp  = reqwest::get("https://httpbin.org/ip")
-    .await?
-    .json::<HashMap<String, String>>()
-    .await?;
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv::dotenv().ok();
+    let cmc_pro_api_key = dotenv::var("CMC_PRO_API_KEY").expect("CMC key not set");
+    let mut params = HashMap::new();
+    params.insert("symbol", "BTC");
 
+    let client = reqwest::Client::new();
+    let resp = client
+    .get("https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest")
+    .header("X-CMC_PRO_API_KEY", cmc_pro_api_key)
+    .query(&params)
+    .send()
+    .await?;
+    let resp  = resp.text().await?;
     println!("{:#?}", resp);
     Ok(())
 }
-
